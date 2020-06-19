9b94bf79c2e2fdca3083c5341f1e6c2f3976c127
John Pendergrass <john.pendergrass@me.com>
2020-06-19 15:38:05 -0500

    made changes to ImperialCard.js and added Nav component folder

--- src/components/ImperialCard/ImperialCard.js
+++ src/components/ImperialCard/ImperialCard.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./ImperialCard.css";
 
 const ImperialCard = (props) => {
+  // eslint-disable-next-line no-unused-expressions
   <div className="card" onClick={() => props.handleClick(props.id)}>
       <div className="image-container">
-          <img className{( props.score === 0 && props.message !== "Click a helmet to begin!") ? "shake" : ""} alt={props.name} src{props.image} /> 
+          <img className={(props.score === 0 && props.message !== "Click a helmet to begin!") ? "shake" : ""} alt={props.name} src={props.image} /> 
       </div>
   </div>
 };
+
+
+export default ImperialCard;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ".Nav.css";
+
+const Nav = props => {
+
+    const messageClass = () => {
+        if (props.message === "The Force is stong with you!") {
+            return "correct";
+        } else if (
+            props.message === "There is a disturbance in the force!") {
+                return "incorrect";
+            }
+    }
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li className="brand">
+                    {props.brand}
+                </li>
+                <li id="message" className={messageClass()}>
+                    {props.message}
+                </li>
+                <li> 
+                    <span className="score"> Score: {props.score}</span> 
+                    <span className="SplitScore">|</span>
+                    <span className="score">Top Score: {props.hiscore}</span>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
