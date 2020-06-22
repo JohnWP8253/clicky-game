918b1298d8883a95111840842d6a799c98d5cdfd
John Pendergrass <john.pendergrass@me.com>
2020-06-22 18:48:43 -0500

    Updates

---
+++ precache-manifest.66f454715430af79477ef66d753e5789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84343237440f278ffb04c667f85c81b0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f0d69570434904e491af",
+    "url": "/clicky-game/static/css/main.e3744b61.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d69570434904e491af",
+    "url": "/clicky-game/static/js/main.e7beefba.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
