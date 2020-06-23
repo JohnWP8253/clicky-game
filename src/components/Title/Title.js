4b649d8cf2b39e487e5d27fca403cb259ed7379b
John Pendergrass <john.pendergrass@me.com>
2020-06-22 19:11:18 -0500

    moved styles from Title.js to Title.css

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Title.css";
 
-const styles = {
-  title: {
-    background: "url('./images/Captain-Phasma.jpg')",
-    backgroundSize: "100% auto",
-    color: "#fff",
-    minHeight: "400px",
-    padding: "180px 20px",
-    paddingBottom: "140px",
-    textAlign: "center",
-    borderBottom: "1px solid #fff"
-  },
-};
+// const styles = {
+//   title: {
+//     background: "url('./images/Captain-Phasma.jpg')",
+//     backgroundSize: "100% auto",
+//     color: "#fff",
+//     minHeight: "400px",
+//     padding: "180px 20px",
+//     paddingBottom: "140px",
+//     textAlign: "center",
+//     borderBottom: "1px solid #fff"
+//   },
+// };
 
 const Title = (props) => (
   
-    <div className="title" style={styles.title}>
+    <div className="title">
       <h1>{props.children}</h1>
       <p className="subtitle">{props.subtitle}</p>
     </div>
